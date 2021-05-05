d1010df67a0bc58eadb809dfbc036638c8e01e70
grnpero <rodrigoalvarez@localhost.localdomain>
2021-05-04 18:40:44 -0700

    Changed from storing data from memory, (array) to storing database using MongoDB Mongoose

--- VIDLY/routes/genres.js
+++ VIDLY/routes/genres.js
@@ -2,35 +2,48 @@ const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
 
-// Our fake database for genres stored in an array
-const genres = [ 
-    { id: 1, name: 'action' },
-    { id: 2, name: 'comedy' },
-    { id: 3, name: 'horror' },
-    { id: 4, name: 'romance' }
-];
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// Creates the Genre schema to be used for the Genre model
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    }
+});
+
+// We create the Genre model to be used for the database
+const Genre = mongoose.model('Genre', genreSchema);
 
 // Shows the user all the available services
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const genres = await Genre
+        .find();
+
     res.send(genres);
 });
 
 // Returns the genre based on the id the user requested
-router.get('/:id', (req, res) => {
-    // Matches the requested id with the id in the array if available
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-    // Returns a 404 if the requested id is not found in the array
-    if (!genre) {
+router.get('/:id', async (req, res) => {
+    // Finds the id of the genre in the MongoDB database
+    try {
+        const genre = await Genre
+            .find({ _id: req.params.id });
+        // If genre with the requested ID is found in the database send a response with the genre
+        res.send(genre);
+    } catch (ex) {
+        // Returns a 404 if the requested id is not found in the database
         return res.status(404).send('The genre with the given ID was not found');
     }
-
-    // If genre with the requested ID is found return the genre
-    res.send(genre);
 });
 
 // Allows the user to add the genre to the genres array with the POST route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // Finds if an error exist 
     const { error } = validateGenre(req.body);
 
@@ -39,25 +52,31 @@ router.post('/', (req, res) => {
         return res.status(400).send(error.details[0].message);
     }
 
-    // Create the genre object based on user request
-    const genre = {
-        id: genres.length + 1,
+    // Create the Genre model object based on user request 
+    const genre = new Genre({
         name: req.body.name
-    };
+    });
 
-    // Push the genre to the array
-    genres.push(genre);
+    // Save to the database if req is valid
+    try {
+        const result = await genre.save();
+        console.log(result);
+    } catch (ex) {
+        for (field in ex.errors) {
+            console.log(ex.errors[field].message)
+        }
+    }
+
     // Send the user created genre back to the user
     res.send(genre);
 });
 
 // Updates the genre based on ID provided by the user
-router.put('/:id', (req, res) => {
-    // Matches the requested id with the id in the array if available
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-    // Returns a 404 if the requested id is not found in the array
-    if (!genre) {
+router.put('/:id', async (req, res) => {
+    // Matches the requested id with the id in the database if available
+    try {
+        const genre = await Genre.findById(req.params.id);
+    } catch (ex) {
         return res.status(404).send('The genre with the given ID was not found');
     }
 
@@ -69,10 +88,24 @@ router.put('/:id', (req, res) => {
         return res.status(400).send(error.details[0].message);
     }
 
-    // Updates the genre name with the user requested genre based on ID
+    // Updates the genre name from the request then saves the changes to the database
     genre.name = req.body.name;
+    const result = await genre.save();
     // Sends the user the genre they submitted
-    res.send(genre);
+    res.send(result);
+});
+
+// Deletes the genre from the database based on the ID the user provided
+router.delete('/:id', async (req, res) => {
+    // Finds if the genre exists in the database
+    try {
+        const genre = await Genre.findByIdAndRemove(req.params.id); 
+        // Return the course that was deleted
+        res.send(genre);
+    } catch (ex) {
+        // If genre not found return 404
+        res.status(404).send('The genre with the given ID was not found!');
+    }
 });
 
 // Validates user input
@@ -85,25 +118,5 @@ function validateGenre(genre) {
     // Returns if their is an error with the user input
     return schema.validate(genre);
 }
-
-// Deletes the genre from the array based on the ID the usere provided
-router.delete('/:id', (req, res) => {
-    // Finds if the genre exists in the genres array
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-    // If genre not found return 404
-    if (!genre) {
-        return res.status(404).send('The genre with the given ID was not found!');
-    }
-
-    // Finds the object based on the index ID
-    const index = genres.indexOf(genre);
-
-    // Remove the genre off the array
-    genres.splice(index, 1);
-
-    // Return the course that was deleted
-    res.send(genre);
-});
 
 module.exports = router;
